046fdf62cbf80ad8a64850538a2a938d61cdf07f
MozyOk <mozy.okubo@gmail.com>
2018-05-23 00:49:39 +0900

    add: new Controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index() {
+        
+        return <<<EOF
+<html>
+<head>
+<title>Hello/Index</titile>
+<style>
+body {font-size: 16px; color:#999; }
+h1 { font-size: 100px; text-aline:right; color:#eee;
+    margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+<h1>Index</h1>
+<p>これは、Helloコントローラのindexアクションです。</p>
+</body>
+</html>
+EOF;
+    }
 }
