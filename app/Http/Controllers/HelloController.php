8a0b8806ae4e5376665b4a5a9eb8b880e9aa1f31
MozyOk <mozy.okubo@gmail.com>
2018-05-23 01:01:25 +0900

    fix: typo

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,16 +11,15 @@ class HelloController extends Controller
         return <<<EOF
 <html>
 <head>
-<title>Hello/Index</titile>
+<title>Hello/Index</title>
 <style>
-body {font-size: 16px; color:#999; }
-h1 { font-size: 100px; text-aline:right; color:#eee;
-    margin:-40px 0px -50px 0px; }
+body {font-size:16px; color:#999; }
+h1 {font-size:100px; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
 </style>
 </head>
 <body>
-<h1>Index</h1>
-<p>これは、Helloコントローラのindexアクションです。</p>
+    <h1>Index</h1>
+    <p>これは、Helloコントローラのindexアクションです。</p>
 </body>
 </html>
 EOF;
